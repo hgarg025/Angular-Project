c364763381f3f58ab5501f1dc698d52097f73683
harshit garg <hgarg_be17@thapar.edu>
2020-06-12 00:29:13 +0530

    first angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-crash-todolist';
+export class AppComponent{
+  url ='https://jsonplaceholder.typicode.com/users';
+  items = []; 
+  constructor(private http: HttpClient) {
+    this.http.get(this.url).toPromise().then(data => {
+      console.log(data);
+
+      for(let key in data)
+      if(data.hasOwnProperty(key))
+      this.items.push(data[key]);
+   });
+   }
+
+  
 }
